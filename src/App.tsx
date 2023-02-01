5b3b0d34fad71c15278c831fdd944cdcc4f70b36
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-02 00:41:42 +0100

    Basic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Note from './components/Note'
+import NoteList from './components/NoteList'
+import NotFound from './components/NotFound'
 
 function App() {
 
   return (
+    <>
 
-    <div className="App">
-      
-    </div>
+    <Navbar />
+
+    <Routes>
+
+        <Route path='/' element={ <Home /> } />
+        <Route path='/note' element={ <Note /> } />
+        <Route path='/notelist' element={ <NoteList /> } />
+        <Route path='/about' element={ <About /> } />
+        <Route path='/contact' element={ <Contact /> } />
+        <Route path='/notfound' element={ <NotFound /> } />
+
+    </Routes>
+    
+    </>
+    
     
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
   return (
 
-    <div>Navbar</div>
-    
+    <nav>
+
+        <ul>
+            <li> <Link to='/' >Home</Link> </li>
+            <li> <Link to='/note' >Note</Link> </li>
+            <li> <Link to='/notelist' >NoteList</Link> </li>
+            <li> <Link to='/about' >About</Link> </li>
+            <li> <Link to='/contact' >Contact</Link> </li>
+            <li> <Link to='/notfound' >NotFound</Link> </li>
+        </ul>
+
+    </nav>
+
   )
 }
 
