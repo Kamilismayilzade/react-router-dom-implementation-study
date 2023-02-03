85efafd1a42b0e4e5d0eeb1efede262b7e8e3d23
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-03 23:23:36 +0100

    linking components and routes

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NoteList = () => {
 
   return (
-
-    <div>NoteList</div>
+    <>
     
+      <div>NoteList</div>
+      <br />
+      <Link to = '/note/1' >Note 1</Link>
+      <br />
+      <Link to = '/note/2' >Note 2</Link>
+      <br />
+      <Link to='/note/new'> New Note </Link>
+      <br />
+    
+    </>
   )
 }
 
