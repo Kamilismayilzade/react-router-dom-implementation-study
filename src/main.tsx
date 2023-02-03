8d099eb52ffc6f1a01b35294556e07c53fed65a9
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-03 23:07:42 +0100

    route types

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter, MemoryRouter } from 'react-router-dom'
+import { StaticRouter } from 'react-router-dom/server'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -11,8 +12,21 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+
+    {/* <HashRouter>
+      <App />
+    </HashRouter>
+
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+    
+    <StaticRouter location='/'>
+      <App />
+    </StaticRouter> */}
     
   </React.StrictMode>,
 
+  
 
 )
