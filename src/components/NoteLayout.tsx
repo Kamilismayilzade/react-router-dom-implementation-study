d718129a771b74f77f91912574728920a30a2cbf
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-04 18:45:03 +0100

    search params

--- src/components/NoteLayout.tsx
+++ src/components/NoteLayout.tsx
@@ -2,33 +2,32 @@ import React, { useState } from 'react'
 import { Link, Outlet, useSearchParams } from 'react-router-dom'
 
 const NoteLayout = () => {
-
-  // const [number, setNumber] = useState(4); 
-  const [searchParams, setSearchParams] = useSearchParams({n: 5});
-    const number = parseInt(searchParams.get('n'), 10);
-    setSearchParams({n: number.toString()});
+  const [searchParams, setSearchParams] = useSearchParams({ n: 5 });
+  const n = searchParams.get("n");
+  const number = n ? parseInt(n, 10) : 5;
 
   return (
     <>
-        <br />
-        <Link to = '/note/1' >Note 1</Link>
-        <br />
-        <Link to = '/note/2' >Note 2</Link>
-        <br />
-        <Link to='/note/new'> New Note </Link>
-        <br />  
+      <br />
+      <Link to="/note/1">Note 1</Link>
+      <br />
+      <Link to="/note/2">Note 2</Link>
+      <br />
+      <Link to="/note/new">New Note</Link>
+      <br />
 
-        <Link to = {`/note/${number}`} >Note {number}</Link>
-        <br />
+      <Link to={`/note/${number}`}>Note {number}</Link>
+      <br />
 
-        <Outlet context={{hello: 'World'}} />
+      <Outlet context={{ hello: "World" }} />
 
-        {/* <input type="number" value={number} onChange={ (e) => setNumber(parseInt(e.target.value)) }  /> */}
-        <input type="number" value={number} onChange={ e => setSearchParams({ n: e.target.value.toString() }) } />
-
-
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setSearchParams({ n: e.target.value.toString() })}
+      />
     </>
-  )
-}
+  );
+};
 
-export default NoteLayout
+export default NoteLayout;
