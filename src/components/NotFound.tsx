517cacc1b90c7f3de8169e5db841cd621a304470
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-04 14:15:34 +0100

    useNavigate hook

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+
+    const navigate = useNavigate();
+
+    useEffect( ()=> {
+
+      setTimeout(()=> {
+        navigate('/');
+      }, 1000)
+
+    } , [])
 
   return (
 
     <div> 404 NotFound</div>
+
+    // <Navigate to='/' />
     
   )
 }
