65d40cf02ed837a45a79966f5540631aa87033fd
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-04 14:07:32 +0100

    Navlink

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
   return (
 
     <nav>
-
+ 
         <ul>
-            <li> <Link to='/' >Home</Link> </li>
+            <li> <Link to='/' replace  >Home</Link> </li>
             <li> <Link to='/note' >Note</Link> </li>
             <li> <Link to='/notelist' >NoteList</Link> </li>
             <li> <Link to='/about' >About</Link> </li>
             <li> <Link to='/contact' >Contact</Link> </li>
             <li> <Link to='/notfound' >NotFound</Link> </li>
+
+            {/* <NavLink to='/' style={ ({isActive})=> {
+              const style = isActive ? {color: 'green'} : {};
+              const text = isActive ? 'Active Home' : 'Home';
+              return (
+                <>
+                  <span style={style}> {text} </span>
+                </>
+              )
+            } } /> */}
+
+            <NavLink end to='/note' ></NavLink>
+
         </ul>
 
     </nav>
