d9b41afea11a9710d7316c44f5e082693c20b544
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-04 14:48:26 +0100

    useSearchParams hook

--- src/components/NoteLayout.tsx
+++ src/components/NoteLayout.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, Outlet, useSearchParams } from 'react-router-dom'
 
 const NoteLayout = () => {
+
+  // const [number, setNumber] = useState(4); 
+  const [searchParams, setSearchParams] = useSearchParams({n: 5});
+    const number = parseInt(searchParams.get('n'), 10);
+    setSearchParams({n: number.toString()});
 
   return (
     <>
@@ -11,9 +16,16 @@ const NoteLayout = () => {
         <Link to = '/note/2' >Note 2</Link>
         <br />
         <Link to='/note/new'> New Note </Link>
+        <br />  
+
+        <Link to = {`/note/${number}`} >Note {number}</Link>
         <br />
 
         <Outlet context={{hello: 'World'}} />
+
+        {/* <input type="number" value={number} onChange={ (e) => setNumber(parseInt(e.target.value)) }  /> */}
+        <input type="number" value={number} onChange={ e => setSearchParams({ n: e.target.value.toString() }) } />
+
 
     </>
   )
