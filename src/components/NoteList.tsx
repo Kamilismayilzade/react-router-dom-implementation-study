6ea70227ea21d6171c0efa5b7e46471ede01b318
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-04 01:35:05 +0100

    fix

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -4,18 +4,9 @@ import { Link } from 'react-router-dom'
 const NoteList = () => {
 
   return (
-    <>
     
       <div>NoteList</div>
-      <br />
-      <Link to = '/note/1' >Note 1</Link>
-      <br />
-      <Link to = '/note/2' >Note 2</Link>
-      <br />
-      <Link to='/note/new'> New Note </Link>
-      <br />
-    
-    </>
+  
   )
 }
 
